f4c3699722fdbf4dfc216c48596a378a67fab83f
sunny <huiseon37@gmail.com>
2022-05-06 22:52:16 +0900

    css: :art: keyframes 수정

--- week3/src/Styles/animation.js
+++ week3/src/Styles/animation.js
@@ -2,19 +2,7 @@ const { keyframes } = require('styled-components');
 
 export const turn = keyframes`
   0% {
-    transform: rotate(60deg);
-  }
-  20% {
-    transform: rotate(120deg);
-  }
-  40% {
-    transform: rotate(180deg);
-  }
-  60% {
-    transform: rotate(240deg);
-  }
-  80% {
-    transform: rotate(300deg);
+    transform: rotate(0deg);
   }
   100% {
     transform: rotate(360deg);
