7f20f42e7c536ff141dbb1165136ecb6003fe5f5
sunny <huiseon37@gmail.com>
2022-05-06 15:10:41 +0900

    feat: :sparkles: 히어로 목록 js파일 생성

---
+++ week3/src/items.js
@@ -0,0 +1,18 @@
+export const items = [
+  {
+    name: '아이언맨',
+    src: 'http://t1.daumcdn.net/thumb/R1024x0/?fname=http://cfile239.uf.daum.net/image/99E0ED425A5873AD0777D7',
+  },
+  {
+    name: '캡틴아메리카',
+    src: 'https://image.bada.io/files//crawling/2021/04/09/bhu/51648357_178b65377e92f4863.gif',
+  },
+  {
+    name: '닥터 스트레인지',
+    src: 'https://w.namu.la/s/ffbf6db5cd247b1661810da201c83aaf16f893da14286670ac23f3f15e68e2722b5fad5c7ccbcfcb49cfa4df70b047236823459ec00d586a16092a0c86b1db6f5b3dce0a4d7cb7f12a7962a77ac83012',
+  },
+  {
+    name: '스칼렛위치',
+    src: 'https://blog.kakaocdn.net/dn/eB1WDK/btrkJ6mp0aY/jQKkhqqTHFRvAK6P6Cy6o0/img.gif',
+  },
+];
