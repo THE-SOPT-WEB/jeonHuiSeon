b8b1266e7426e4d9c5caded7ab5fb9e2847eb5db
sunny <huiseon37@gmail.com>
2022-05-06 15:13:11 +0900

    feat: :sparkles: game 페이지 구현

--- week3/src/Pages/Game.jsx
+++ week3/src/Pages/Game.jsx
@@ -1,7 +1,29 @@
+import ItemSelector from 'components/ItemSelector';
 import React from 'react';
+import styled from 'styled-components';
+import { theme } from 'styles/theme';
 
 function Game() {
-  return <div>Game</div>;
+  return (
+    <StyledRoot>
+      <h1>마블 히어로 월드컵</h1>
+      <ItemSelector />
+    </StyledRoot>
+  );
 }
 
 export default Game;
+
+const StyledRoot = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  background-color: ${theme.colors.main};
+
+  & > h1 {
+    font-size: 5rem;
+    margin: 3rem 0;
+    color: white;
+  }
+`;
